39e6b4146b9e3add0afa3805afc883ea55384518
SOFY AI <86354806+SOFYAIOFFICIAL@users.noreply.github.com>
2021-07-29 17:38:45 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 set -ex
 echo "build path "${build_path}""
 
-curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+curl --location --request POST 'https://api.sofy.ai/api/AppTests/buildUpload' \
 --header "SubscriptionKey: ${subscription_key}" \
 --form "applicationFile=@"${build_path}""
