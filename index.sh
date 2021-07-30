08d35356648b82de01d0809ac31a1efdb9b90993
SOFY AI <86354806+SOFYAIOFFICIAL@users.noreply.github.com>
2021-07-30 14:46:30 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 set -ex
 echo "build path "${build_path}""
-
+echo "${github.action_path }"
 
 curl --location --request POST 'https://api.sofy.ai/api/AppTests/buildUpload' \
 --header "SubscriptionKey: ${subscription_key}" \
